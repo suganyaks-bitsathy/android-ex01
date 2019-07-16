5d097feb31128a833702e2803cd4d4cff424f600
suganyaks-bitsathy <30622465+suganyaks-bitsathy@users.noreply.github.com>
2019-07-16 10:03:34 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.simpleusermessage;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    String username="admin";
+    String password="admin123";
+    EditText editextuser,editextpass;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void checklogin(View view) {
+        editextuser=(EditText)findViewById(R.id.etuser);
+        editextpass=(EditText)findViewById(R.id.etpass);
+        btn=(Button)findViewById(R.id.btnok);
+        if(editextuser.getText().toString().equalsIgnoreCase(username) && editextpass.getText().toString().equals(password)){
+            Toast.makeText(getApplicationContext(),"valid user",Toast.LENGTH_LONG).show();
+        }else
+            Toast.makeText(getApplicationContext(),"Invalid user",Toast.LENGTH_LONG).show();
+    }
+}
